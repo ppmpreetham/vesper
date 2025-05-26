788b2cccda28d99d7dd30d6ea59e91df395f99a2
preetham <ppmpreetham1@gmail.com>
2025-05-26 10:29:58 +0000

    →add: sites

---
+++ sites/sites.go
@@ -0,0 +1,22 @@
+package sites
+
+var DefaultSites = []Site{
+	{
+		Name:           "Twitter",
+		UrlTemplate:    "https://x.com/%s",
+		UsernameRegex:  `^[A-Za-z0-9_]{1,15}$`,
+		ErrorMsg:       "Sorry, that page does not exist!",
+		Method:         "GET",
+		DisplayURL:     "https://twitter.com/%s",
+		ErrorRegex:     `Sorry, that page does not exist!`,
+		TimeoutSeconds: 10,
+	},
+	{
+		Name:          "GitHub",
+		UrlTemplate:   "https://githib.com/%s",
+		UsernameRegex: `^[A-Za-z0-9][A-Za-z0-9-]{0,38}$`,
+		ErrorMsg:      "This account doesn't exist",
+		Method:        "GET",
+		ErrorRegex:    `This account doesn't exist`,
+	},
+}
